5d0f5a11f875f92a462558d43cea7d209c7c191c
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2025-05-26 15:19:49 +0100

    feat: add dualopt

---
+++ DistributedSim/gradient_strategy/dualopt_gradient.py
@@ -0,0 +1,80 @@
+from copy import deepcopy
+
+import torch.distributed as dist
+from torch.nn import utils as nn_utils
+
+from .communicate import *
+from .gradient_strategy import GradientStrategy
+
+
+class DualOptGradient(GradientStrategy):
+    def __init__(self, rank, model, config, logger=None):
+        super().__init__(rank, model, config, logger)
+
+        if self.rank == 0:
+            self.master_model = deepcopy(model).to("cpu")
+            for param in self.master_model.parameters():
+                param.requires_grad = True
+
+            self.outer_optimizer = self.gradient_config.outer_optimizer_cls(
+                self.master_model.parameters(),
+                **self.gradient_config.outer_optimizer_kwargs,
+            )
+            # Store a snapshot of master model's parameters before local steps
+            self._master_params_before = [
+                p.data.clone() for p in self.master_model.parameters()
+            ]
+
+        self.optim = self.gradient_config.optimizer_class(
+            model.parameters(), **self.gradient_config.optimizer_kwargs
+        )
+        self._setup_scheduler()
+
+    def _average_models(self) -> None:
+        for param in self.model.parameters():
+            all_reduce(param.data, op=dist.ReduceOp.SUM)
+            param.data /= self.config.num_nodes
+
+    def _broadcast_model_params(self) -> None:
+        for param in self.model.parameters():
+            broadcast(param.data, src=0)
+
+    def _set_master_grad_from_diff(self, before_params):
+        for param, before in zip(self.master_model.parameters(), before_params):
+            param.grad = before - param.data
+
+    def _synchronize_master_model(self) -> None:
+        for name, param in self.model.named_parameters():
+            param.data = self.master_model.state_dict()[name].data.to(param.device)
+
+    def step(self):
+        if self.gradient_config.max_norm:
+            nn_utils.clip_grad_norm_(
+                self.model.parameters(), max_norm=self.gradient_config.max_norm
+            )
+
+        # Local (inner) step
+        self.optim.step()
+
+        # At the end of the interval, update master model using diff
+        if (
+            self.local_step % self.gradient_config.diloco_interval == 0
+            and self.local_step > 0
+        ):
+            if self.rank == 0:
+                # Compute gradient as difference between before and after local steps
+                self.outer_optimizer.zero_grad()
+                self._set_master_grad_from_diff(self._master_params_before)
+                self.outer_optimizer.step()
+                # Update snapshot for next interval
+                self._master_params_before = [
+                    p.data.clone() for p in self.master_model.parameters()
+                ]
+                # Synchronize master model to local model
+                self._synchronize_master_model()
+
+            # Average and broadcast updated master model
+            self._average_models()
+            self._broadcast_model_params()
+
+        super().step()
---
+++ run/nanogpt_dualopt.py
@@ -0,0 +1,40 @@
+import torch
+from nanogpt import arg_parse, config_gen, gen_gpt_config
+
+from DistributedSim.dataset.nanogpt.build_dataset import *
+from DistributedSim.gradient_strategy.dualopt_gradient import DualOptGradient
+from DistributedSim.gradient_strategy.gradient_strategy import *
+from DistributedSim.sim_builder import *
+from DistributedSim.sim_config import *
+
+
+def main():
+    parser = arg_parse()
+
+    parser.add_argument("--diloco_interval", type=int, default=100)
+    parser.add_argument("--outer_lr", type=float, default=0.7)
+    parser.add_argument("--nesterov", type=bool, default=True)
+    parser.add_argument("--outer_momentum", type=float, default=0.9)
+
+    args = parser.parse_args()
+
+    gpt_config = gen_gpt_config(args)
+
+    config = config_gen(args, gpt_config)
+
+    config.gradient_class = DualOptGradient
+    config.gradient_config.diloco_interval = args.diloco_interval
+    config.gradient_config.outer_optimizer_cls = torch.optim.SGD
+    config.gradient_config.outer_optimizer_kwargs = {
+        "lr": args.outer_lr,
+        "nesterov": args.nesterov,
+        "momentum": args.outer_momentum,
+    }
+
+    simbuilder = LocalSimBuilder(config)
+
+    simbuilder.execute()
+
+
+if __name__ == "__main__":
+    main()
